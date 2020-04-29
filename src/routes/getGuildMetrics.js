e884c7bf0cc181e2061e89e3a227d4ac4539014b
Alexis Tan <alexis@sen-dev.com>
2020-04-30 00:29:49 +0200

    Add average skill with progress to route resposnes

--- src/routes/getGuildMetrics.js
+++ src/routes/getGuildMetrics.js
@@ -17,7 +17,7 @@ module.exports = async (request, response) => {
     guild = guild.pop();
 
     let metrics = await app.database.getClient()
-        .select('average_skill', 'average_slayer', 'members', 'created_at')
+        .select('average_skill', 'average_skill_progress', 'average_slayer', 'members', 'created_at')
         .from('metrics')
         .where('guild_id', guild.id)
         .orderBy('created_at', 'desc');
