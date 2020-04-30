7f0382cd3ddc5f0c4bb402afb9ae407c4b820874
Alexis Tan <alexis@sen-dev.com>
2020-04-30 16:21:32 +0200

    Add guild name to players output

--- src/routes/getPlayers.js
+++ src/routes/getPlayers.js
@@ -2,11 +2,17 @@
 const app = require('../application');
 
 module.exports = async (request, response) => {
-    let players = await app.database.getClient().from('players').orderBy('username');
+    let players = await app.database.getClient()
+        .select(['guilds.name as guild_name', 'players.*'])
+        .from('players')
+        .leftJoin('guilds', function () {
+            return this.on('players.guild_id', '=', 'guilds.id');
+        });
 
     response.json({
         status: 200,
         data: players.map(player => {
+            delete player.guild_id;
             delete player.created_at;
             delete player.updated_at;
 
