cd209507e8dcdb4ad7f70b443614b028fafcaaa2
Alexis Tan <alexis@sen-dev.com>
2020-06-22 20:41:52 +0200

    Add support for sorting guild lists

--- web/assets/js/views/guilds/ListGuilds.vue
+++ web/assets/js/views/guilds/ListGuilds.vue
@@ -20,10 +20,22 @@
                         <thead>
                             <tr>
                                 <th>#</th>
-                                <th>Name</th>
-                                <th>Members</th>
-                                <th>Average Skills</th>
-                                <th>Average Slayer</th>
+                                <th class="is-clickable" @click="clickSort('name')">
+                                    Name
+                                    <sort-button v-show="sorter == 'name'" :sort="this.reverseSort" />
+                                </th>
+                                <th class="is-clickable" @click="clickSort('members')">
+                                    Members
+                                    <sort-button v-show="sorter == 'members'" :sort="this.reverseSort" />
+                                </th>
+                                <th class="is-clickable" @click="clickSort('average_skill_progress')">
+                                    Average Skills
+                                    <sort-button v-show="sorter == 'average_skill_progress'" :sort="this.reverseSort" />
+                                </th>
+                                <th class="is-clickable" @click="clickSort('average_slayer')">
+                                    Average Slayer
+                                    <sort-button v-show="sorter == 'average_slayer'" :sort="this.reverseSort" />
+                                </th>
                                 <th>Last updated</th>
                             </tr>
                         </thead>
@@ -43,7 +55,7 @@
                                 <td>
                                     <span class="tag is-danger">{{ formatNumber(guild.average_slayer) }}</span>
                                 </td>
-                                <td>{{ guild.last_updated_at }}</td>
+                                <td>{{ guild.last_updated_at_humanized }}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -60,11 +72,33 @@
 </style>
 
 <script>
+    import SortButton from '../../components/SortButton';
     import Store from '../../store';
     import moment from 'moment';
 
     export default {
+        components: {
+            SortButton,
+        },
+        data() {
+            return {
+                sorter: 'average_skill_progress',
+                reverseSort: false,
+            };
+        },
         methods: {
+            clickSort(method, reverse = null) {
+                if (reverse == null) {
+                    reverse = this.reverseSort;
+                }
+
+                if (this.sorter == method) {
+                    reverse = ! reverse;
+                }
+
+                this.sorter = method;
+                this.reverseSort = reverse;
+            },
             clickGuild(guild) {
                 console.log(guild);
             }
@@ -75,9 +109,15 @@
             },
             guilds() {
                 return Store.get('guilds').sort((v1, v2) => {
-                    return v2.average_skill_progress - v1.average_skill_progress;
+                    if (this.sorter == null) {
+                        return v2.average_skill_progress - v1.average_skill_progress;
+                    }
+
+                    return this.reverseSort
+                        ? v1[this.sorter] > v2[this.sorter] ? 1 : -1
+                        : v2[this.sorter] > v1[this.sorter] ? 1 : -1;
                 }).map(guild => {
-                    guild.last_updated_at = moment(guild.last_updated_at)
+                    guild.last_updated_at_humanized = moment(guild.last_updated_at)
                         .toNow()
                         .split(' ')
                         .splice(1)
