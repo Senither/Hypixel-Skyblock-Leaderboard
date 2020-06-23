27cc3932bd1281eaa7e6b7be5060dd071161820a
Alexis Tan <alexis@sen-dev.com>
2020-06-23 12:09:43 +0200

    Remove dumb code

--- web/assets/js/views/App.vue
+++ web/assets/js/views/App.vue
@@ -25,45 +25,6 @@
 </template>
 
 <script>
-    import LoadingReport from './LoadingReport';
-    import Report from './Report';
-    export default {
-        components: {
-            LoadingReport,
-            Report
-        },
-        mounted() {
-            this.id = window.location.href.split('/').pop();
-            this.stage = this.stages.LOADING_REPORT;
-        },
-        data() {
-            return {
-                report: null,
-                stage: null,
-                stages: {
-                    LOADING_REPORT: 0,
-                    HAS_REPORT: 1,
-                },
-                id: null,
-            };
-        },
-        methods: {
-            handleLoadedReport(event) {
-                this.report = event.report;
-                this.stage = this.stages.HAS_REPORT;
-            }
-        }
-    };
-</script>
-
-                    </p>
-                </div>
-            </footer>
-        </transition-page>
-    </div>
-</template>
-
-<script>
     import TransitionPage from '../components/TransitionPage';
     import Loading from '../components/Loading';
 
