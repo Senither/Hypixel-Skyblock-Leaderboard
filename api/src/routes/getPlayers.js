0600d344bfced52b228a61071f85ceb30776b797
Alexis Tan <alexis@sen-dev.com>
2020-08-18 22:38:51 +0200

    Add support for searching for players

--- api/src/routes/getPlayers.js
+++ api/src/routes/getPlayers.js
@@ -2,13 +2,20 @@
 const app = require('../application');
 
 module.exports = async (request, response) => {
-    let players = await app.database.getClient()
+    let query = app.database.getClient()
         .select(['guilds.name as guild_name', 'players.*'])
         .from('players')
         .leftJoin('guilds', function () {
             return this.on('players.guild_id', '=', 'guilds.id');
         })
         .havingRaw('guilds.name IS NOT ?', [null]);
+
+    const username = request.query.username;
+    if (username != null && username != undefined) {
+        query.where('username', 'like', `%${username}%`);
+    }
+
+    players = await query;
 
     response.json({
         status: 200,
