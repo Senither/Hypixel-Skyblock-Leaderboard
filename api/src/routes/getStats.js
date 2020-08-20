9d0b4122ef156e805e67341d1d34286433f1a7d2
Alexis Tan <alexis@sen-dev.com>
2020-08-20 14:51:34 +0200

    Add metrics counters to the stats route response
    This also adds a simple in-memory caching to help speed up the route a bit.

--- api/src/routes/getStats.js
+++ api/src/routes/getStats.js
@@ -1,15 +1,64 @@
 
 const app = require('../application');
 
-module.exports = async (request, response) => {
-    let guilds = await app.database.getClient().table('guilds').count({ total: 'id' });
-    let players = await app.database.getClient().table('players').count({ total: 'uuid' });
+/**
+ * The local in-memory cache object that
+ * stores all of our cache entities.
+ *
+ * @type {Object}
+ */
+const cache = {};
 
+/**
+ * Gets the given entiry fingerprint from the in-memory cache if it exists,
+ * otherwise it will use the callback function to retrive the value and
+ * store it back into the cache.
+ *
+ * @param  {String}   fingerprint
+ * @param  {Function} callback
+ * @return {Integer}
+ */
+async function getCacheEntity(fingerprint, callback) {
+    if (cache.hasOwnProperty(fingerprint)) {
+        return cache[fingerprint];
+    }
+
+    const result = await callback();
+
+    cache[fingerprint] = result.pop().total;
+    setTimeout(() => delete cache[fingerprint], 60000);
+
+    return cache[fingerprint];
+}
+
+/**
+ * Creates the SQL query that should be used to retrive the counter metrics.
+ *
+ * @param  {String} table
+ * @param  {String} column
+ * @return {Object}
+ */
+async function createCounterQuery(table, column) {
+    return app.database.getClient().table(table).count({ total: column });
+}
+
+/**
+ * Handles the request by retriving the metrics from the cache,
+ * or querying the data from the database if it does
+ * not exist in the cache.
+ *
+ * @param  {Object} request
+ * @param  {Object} response
+ * @return {Object}
+ */
+module.exports = async (request, response) => {
     response.json({
         status: 200,
         data: {
-            guilds: guilds.pop().total,
-            players: players.pop().total,
+            guilds: await getCacheEntity('guilds', async () => createCounterQuery('guilds', 'id')),
+            guilds_metrics: await getCacheEntity('guildMetrics', async () => createCounterQuery('metrics', 'id')),
+            players: await getCacheEntity('players', async () => createCounterQuery('players', 'uuid')),
+            players_metrics: await getCacheEntity('playerMetrics', async () => createCounterQuery('player_metrics', 'id')),
         }
     });
 };
