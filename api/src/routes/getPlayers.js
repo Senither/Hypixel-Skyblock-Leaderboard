62a71c0a9ecbfec096e68add4851a5c3daca96a5
Alexis Tan <alexis@sen-dev.com>
2020-08-22 14:20:32 +0200

    Add pagination for the players API route

--- api/src/routes/getPlayers.js
+++ api/src/routes/getPlayers.js
@@ -1,6 +1,36 @@
 
 const app = require('../application');
 
+function has(request, name) {
+    let prop = request.query[name];
+    return prop != null && prop != undefined;
+}
+
+async function paginateResponse(request, query) {
+    let perPage = parseInt(request.query.perPage);
+    let page = 1;
+
+    let total = await app.database.getClient().from('players').count({ total: 'uuid' });
+    let totalEntities = total[0].total;
+    let lastPage = Math.floor(totalEntities / perPage);
+
+    query.limit(perPage);
+
+    if (has(request, 'page')) {
+        page = Math.max((parseInt(request.query.page) - 1), 1);
+        page = Math.min(page, lastPage);
+
+        query.offset((page - 1) * perPage);
+    }
+
+    return {
+        current_page: page,
+        last_page: lastPage,
+        per_page: perPage,
+        total: totalEntities,
+    };
+}
+
 module.exports = async (request, response) => {
     let query = app.database.getClient()
         .select(['guilds.name as guild_name', 'players.*'])
@@ -10,21 +40,29 @@ module.exports = async (request, response) => {
         })
         .havingRaw('guilds.name IS NOT ?', [null]);
 
-    const username = request.query.username;
-    if (username != null && username != undefined) {
-        query.where('username', 'like', `%${username}%`);
+    if (has(request, 'username')) {
+        query.where('username', 'like', `%${request.query.username}%`);
     }
 
-    players = await query;
+    let paginate = null;
+    if (has(request, 'perPage')) {
+        paginate = await paginateResponse(request, query);
+    }
 
-    response.json({
+    let jsonResponse = {
         status: 200,
-        data: players.map(player => {
+        data: (await query).map(player => {
             delete player.guild_id;
             delete player.created_at;
             delete player.updated_at;
 
             return player;
         })
-    });
+    };
+
+    if (paginate != null) {
+        jsonResponse.paginate = paginate;
+    }
+
+    response.json(jsonResponse);
 };
