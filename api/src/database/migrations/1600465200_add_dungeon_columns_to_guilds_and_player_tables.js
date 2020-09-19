9d03f6642337ec881767445d3a189bb5227f54da
Alexis Tan <alexis@sen-dev.com>
2020-09-19 13:04:34 +0200

    Fix dungeon columns being able to store full length values

--- api/src/database/migrations/1600465200_add_dungeon_columns_to_guilds_and_player_tables.js
+++ api/src/database/migrations/1600465200_add_dungeon_columns_to_guilds_and_player_tables.js
@@ -21,37 +21,37 @@ exports.up = function (knex) {
             table.decimal('catacomb')
                  .defaultTo(0)
                  .after('average_skill_progress');
-            table.decimal('catacomb_xp')
+            table.decimal('catacomb_xp', 12)
                  .defaultTo(0)
                  .after('catacomb');
             table.decimal('healer')
                  .defaultTo(0)
                  .after('catacomb_xp');
-            table.decimal('healer_xp')
+            table.decimal('healer_xp', 12)
                  .defaultTo(0)
                  .after('healer');
             table.decimal('mage')
                  .defaultTo(0)
                  .after('healer_xp');
-            table.decimal('mage_xp')
+            table.decimal('mage_xp', 12)
                  .defaultTo(0)
                  .after('mage');
             table.decimal('berserk')
                  .defaultTo(0)
                  .after('mage_xp');
-            table.decimal('berserk_xp')
+            table.decimal('berserk_xp', 12)
                  .defaultTo(0)
                  .after('berserk');
             table.decimal('archer')
                  .defaultTo(0)
                  .after('berserk_xp');
-            table.decimal('archer_xp')
+            table.decimal('archer_xp', 12)
                  .defaultTo(0)
                  .after('archer');
             table.decimal('tank')
                  .defaultTo(0)
                  .after('archer_xp');
-            table.decimal('tank_xp')
+            table.decimal('tank_xp', 12)
                  .defaultTo(0)
                  .after('tank');
         }),
@@ -59,37 +59,37 @@ exports.up = function (knex) {
             table.decimal('catacomb')
                  .defaultTo(0)
                  .after('average_skill_progress');
-            table.decimal('catacomb_xp')
+            table.decimal('catacomb_xp', 12)
                  .defaultTo(0)
                  .after('catacomb');
             table.decimal('healer')
                  .defaultTo(0)
                  .after('catacomb_xp');
-            table.decimal('healer_xp')
+            table.decimal('healer_xp', 12)
                  .defaultTo(0)
                  .after('healer');
-            table.decimal('mage')
+            table.decimal('mage',)
                  .defaultTo(0)
                  .after('healer_xp');
-            table.decimal('mage_xp')
+            table.decimal('mage_xp', 12)
                  .defaultTo(0)
                  .after('mage');
             table.decimal('berserk')
                  .defaultTo(0)
                  .after('mage_xp');
-            table.decimal('berserk_xp')
+            table.decimal('berserk_xp', 12)
                  .defaultTo(0)
                  .after('berserk');
             table.decimal('archer')
                  .defaultTo(0)
                  .after('berserk_xp');
-            table.decimal('archer_xp')
+            table.decimal('archer_xp', 12)
                  .defaultTo(0)
                  .after('archer');
             table.decimal('tank')
                  .defaultTo(0)
                  .after('archer_xp');
-            table.decimal('tank_xp')
+            table.decimal('tank_xp', 12)
                  .defaultTo(0)
                  .after('tank');
         }),
